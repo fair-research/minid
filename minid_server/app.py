0d98e084c2e6eee5296df24c57230595d8ddc11d
Nickolaus D. Saint <nick@globus.org>
2017-11-28 08:39:49 -0800

    Changed app to support 'local_config.py'

--- minid_server/app.py
+++ minid_server/app.py
@@ -1,11 +1,15 @@
 #!flask/bin/python
+import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from email_sender import SESEmail
 
 app = Flask(__name__)
-app.config.from_object('config.BaseConfig')
+if os.path.exists('local_config.py'):
+    app.config.from_object('local_config.Config')
+else:
+    app.config.from_object('config.Config')
+    print('Warning: Could not find "local_config", you may need to create it '
+          'and supply required secret credentials (like AWS mail).')
 db = SQLAlchemy(app)
 minid_email = SESEmail(app)
-
-print("Running")
