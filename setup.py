2a8154bb19dd74c9e88a927d51c2463724344444
Nickolaus D. Saint <nick@globus.org>
2020-03-12 15:20:40 -0700

    Changed author from kyle to fair research

--- setup.py
+++ setup.py
@@ -22,11 +22,10 @@ with open('requirements.txt') as reqs:
 setup(
     name='minid',
     version=version['__VERSION__'],
-    description='BD2K Minimum Viable Identifier',
+    description='FAIR Research Minimum Viable Identifier',
     long_description=long_description,
     url='https://fair-research.org/',
-    author='Kyle Chard',
-    author_email='chard@uchicago.edu',
+    author="FAIR Research Team",
     packages=find_packages(),
     install_requires=install_requires,
     license='Apache 2.0',
